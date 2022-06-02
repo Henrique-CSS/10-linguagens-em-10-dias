38f1550a5a5ecb33165e906a5b3ab26f38a5bbc1
henrique-css <henriqeusoliveira05@gmail.com>
2022-06-02 11:48:08 -0300

    master

--- php/compiler.php
+++ php/compiler.php
@@ -1,5 +1,23 @@
 <?php
 
 $fileName = $argv[1];
-file_get_contents($fileName);
+$code = file_get_contents($fileName);
+$count = 0;
 
+while ($count < strlen($code)) {
+    if ($code[++$count] = "p") {
+        if ($code[++$count] = "r") {
+            if ($code[++$count] = "i") {
+                if ($code[++$count] = "n") {
+                    if ($code[++$count] = "t") {
+                        if ($code[$count] = "(") {
+                            while (isset($code[++$count]) and $code[$count] != ")") {
+                                echo $code[$count];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
