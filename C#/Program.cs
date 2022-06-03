c444ba11b28ea6315acaf75791b072c3bceab74c
henrique-css <henriqeusoliveira05@gmail.com>
2022-06-03 13:52:05 -0300

    master

---
+++ C#/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+string[] file = File.ReadAllLines("C:/<pathto>/test7.txt");
+
+foreach(string line in file)
+{
+    foreach(char letter in line)
+    {
+         if (letter == 'T')
+         {
+                // do something
+         }
+    }
+}
